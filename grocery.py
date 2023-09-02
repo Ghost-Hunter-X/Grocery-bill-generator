89e56dabda31440191b9711178a4b9944f292848
Ghost-Hunter-X <82134198+Ghost-Hunter-X@users.noreply.github.com>
2023-09-02 14:36:52 +0530

    Add files via upload

--- grocery.py
+++ grocery.py
@@ -1,5 +1,25 @@
-print("|------------------------|","| Grocery Bill Generator |","|------------------------|", sep='\n')
-print("|------------|","| Store Name |","|------------|", sep='\n')
+#Variable that can be changed
+store_name="Store Namefkcyhuifwfihkjjj"
+#Make item and cost var here
+name_3,cost_3="₹1 Items",1
+name_4,cost_4="₹2 Items",2
+name_5,cost_5="₹5 Items",5
+name_6,cost_6="₹10 Items",10
+name_7,cost_7="₹20 Items",20
+name_8,cost_8="₹30 Items",30
+name_9,cost_9="₹50 Items",50
+
+
+import json
+import os
+print('|','-'*22,'|')
+print("| Grocery Bill Generator |")
+print("|","-"*22,"|")
+print()
+print("|","-"*len(store_name),"|")
+print("|",store_name,"|")
+print("|","-"*len(store_name),"|")
+print()
 def exline(s):
     s.writelines('\n')
 import datetime
@@ -7,10 +27,15 @@ print(str(datetime.datetime.now()))
 z=0
 e=0
 x=0
-a={1:"",2:"",3:"₹1 Items",4:"₹2 Items",5:"₹5 Items",6:"₹10 Items",7:"₹20 Items",8:"₹30 Items",9:"₹50 Items"}
+a={1:"",2:"",3:name_3,4:name_4,5:name_5,6:name_6,
+   7:name_7,8:name_8,9:name_9}
 b={}
 while x in range(0,12):
-    print("1:History","2:clear current","3:1₹ Items","4:2₹ Items","5:5₹ Items","6:10₹ Items","7:20₹ Items","8:30₹ Items","9:50₹ Items","10:Print Bill","0:Custom item","Or Any other key to exit..", sep = '     ')
+    print_list=("Custom item","History","clear current",name_3,name_4,name_5,name_6,name_7,name_8
+                ,name_9,"Print Bill")
+    for index, com in enumerate(print_list):
+        print(index,com)
+    print("Or Any other key to exit..")
     x=(input("Select item number you want to add to bill:-"))
     if x.isnumeric():
         x=int(x)
@@ -26,7 +51,7 @@ while x in range(0,12):
             if j=="y":
                 nam=str(input("Name of customer: "))
                 print("Saving...")
-                history = open('History.pickle','a+' , encoding="utf-8")
+                history = open('History.txt','a' , encoding="utf-8")
                 history.writelines(nam)
                 exline(history)
                 now = datetime.datetime.now()
@@ -45,28 +70,28 @@ while x in range(0,12):
                 for xx in range(0,4):
                     exline(history)
                     if xx==1:
-                        history.writelines("-------------------------")
+                        history.writelines("-"*24)
                     xx=xx+1
                 history.writelines
-                history.close
+                history.close()
                 print("Saved!""\n""\n")
             elif j=="n" or j.isalnum():
                 pass
         elif x in range(3,10): #Pre defined prices
             if x==3:
-                c=1
+                c=cost_3
             elif x==4:
-                c=2
+                c=cost_4
             elif x==5:
-                c=5
+                c=cost_5
             elif x==6:
-                c=10
+                c=cost_6
             elif x==7:
-                c=20
+                c=cost_7
             elif x==8:
-                c=30
+                c=cost_8
             elif x==9:
-                c=50
+                c=cost_9
             y=float(input("Enter quantity of selected item or Weight(in KG):-"))
             d=(c*y)
             z=(z)+(d)
@@ -93,8 +118,9 @@ while x in range(0,12):
             print()
             print("-------------------------")
             print()
-            r=open("History.pickle","r+" ,encoding="utf-8")
-            with open("History.pickle","r+",encoding="utf-8") as f:
+            print(os.path.abspath("History.txt"))
+            r=open("History.txt","r+" ,encoding="utf-8", errors='ignore')
+            with open("History.txt","r+",encoding="utf-8", errors='ignore') as f:
                 for i in r:
                     print(i.strip('\n'))
         elif x==2: #Clear current bill
@@ -113,7 +139,8 @@ while x in range(0,12):
         w=input("Are you sure you want to exit?  y=yes OR n=no : ")
         w=w.lower()
         if w=="y":
-            print("Thank you for using our software!")
+            print()
+            print('Thank you for using our software!')
             break
         else:
             x=0
